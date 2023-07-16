6699c07b4d6c71f49fd04fd5be9e2afd6b57c809
Joseph Endale <josi.endal45@gmail.com>
2023-07-16 12:43:49 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,35 +1,58 @@
 #!/usr/bin/python3
+
 """
-This script converts a markdown to html and prints it out.
+markdown2html.py - A script to convert a markdown file to HTML.
+
+Usage: markdown2html.py <input_file> <output_file>
 """
 
 import sys
 import mistune
 
-# check if correct number of arguments provided
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+
+def print_usage_and_exit():
+    """Print usage message to STDERR and exit with status 1."""
+
+    print("Usage: markdown2html.py <input_file> <output_file>", file=sys.stderr)
     sys.exit(1)
 
-# get input and output file names from command line arguments
-input_file = sys.argv[1]
-output_file = sys.argv[2]
 
-try:
-    # read input file
-    with open(input_file, "r") as f:
-        input_text = f.read()
-except FileNotFoundError:
-    # input file not found, print error and exit with status 1
-    print("Missing", input_file, file=sys.stderr)
+def print_missing_file_and_exit(filename):
+    """Print missing file error message to STDERR and exit with status 1."""
+
+    print(f"Missing {filename}", file=sys.stderr)
     sys.exit(1)
 
-# convert markdown to html
-html_text = mistune.markdown(input_text)
 
-# write html to output file
-with open(output_file, "w") as f:
-    f.write(html_text)
+def main():
+    """Entry point of the script"""
 
-# exit with status 0
-sys.exit(0)
+    # check if correct number of arguments provided
+    if len(sys.argv) != 3:
+        print_usage_and_exit()
+
+    # get input and output file names from command line arguments
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    try:
+        # read input file
+        with open(input_file, "r") as f:
+            input_text = f.read()
+    except FileNotFoundError:
+        # input file not found, print error and exit with status 1
+        print_missing_file_and_exit(input_file)
+
+    # convert markdown to html
+    html_text = mistune.markdown(input_text)
+
+    # write html to output file
+    with open(output_file, "w") as f:
+        f.write(html_text)
+
+    # exit with status 0
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
