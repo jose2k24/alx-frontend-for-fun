be786533cb09fc79c33e928229749d82bc4fa2c5
Joseph Endale <josi.endal45@gmail.com>
2023-12-15 21:26:33 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,43 +1,123 @@
 #!/usr/bin/python3
-'''
-A script that codes markdown to HTML
-'''
+""" 
+Script that takes an argument 2 strings:
+
+First argument is the name of the Markdown file
+Second argument is the output file name
+"""
+
+import hashlib
 import sys
 import os
-import re
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
+    if not os.path.exists(sys.argv[1]):
+        sys.stderr.write("Missing " + sys.argv[1] + "\n")
+        exit(1)
 
-    # Test that the number of arguments passed is 2
-    if len(sys.argv[1:]) != 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
+    with open(sys.argv[1], 'r') as r:
+        with open(sys.argv[2], 'w') as w:
+            ol_status = False
+            ul_status = False
+            p_status = False
+            for line in r:
 
-    # Store the arguments into variables
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+                # initialize counters for bold and emphasis
+                bold_counter = 0
+                emphasis_counter = 0
 
-    # Checks that the markdown file exists and is a file
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f'Missing {input_file}', file=sys.stderr)
-        sys.exit(1)
+                # replace ** with <b> and </b>
+                while '**' in line:
+                    if bold_counter % 2 == 0:
+                        line = line.replace('**', '<b>', 1)
+                    else:
+                        line = line.replace('**', '</b>', 1)
+                    bold_counter += 1
 
-    with open(input_file, encoding='utf-8') as file_1:
-        html_content = []
-        md_content = [line[:-1] for line in file_1.readlines()]
-        for line in md_content:
-            heading = re.split(r'#{1,6} ', line)
-            if len(heading) > 1:
-                # Compute the number of the # present to
-                # determine heading level
-                h_level = len(line[:line.find(heading[1])-1])
-                # Append the html equivalent of the heading
-                html_content.append(
-                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
-                )
-            else:
-                html_content.append(line)
+                # replace __ with <em> and </em>
+                while '__' in line:
+                    if emphasis_counter % 2 == 0:
+                        line = line.replace('__', '<em>', 1)
+                    else:
+                        line = line.replace('__', '</em>', 1)
+                    emphasis_counter += 1
+                
+                # convert in MD5
+                while '[[' in line and ']]' in line:
+                    start = line.find('[[')
+                    end = line.find(']]')
+                    if start < end:
+                        text = line[start + 2:end]
+                        md5_text = hashlib.md5(text.encode()).hexdigest()
+                        line = line[:start] + md5_text + line[end + 2:]
 
-    with open(output_file, 'w', encoding='utf-8') as file_2:
-        file_2.writelines(html_content)
+                # remove all c
+                while '((' in line and '))' in line:
+                    start = line.find('((')
+                    end = line.find('))')
+                    if start < end:
+                        text = line[start + 2:end]
+                        text_without_c = text.replace('c', '').replace('C', '')
+                        line = line[:start] + text_without_c + line[end + 2:]
+
+                length = len(line)
+
+                # strip to get tag
+                h = line.lstrip('#')
+                hlevel = line.count('#')
+                ol = line.lstrip('*')
+                ol_element = length - len(ol)
+                ul = line.lstrip('-')
+                ul_element = length - len(ul)
+
+                # headers
+                if 1 <= hlevel <= 7:
+                    line = f"<h{hlevel}>{h.strip()}</h{hlevel}>\n"
+
+                # ordered list
+                if ol_element:
+                    if not ol_status:
+                        w.write('<ol>\n')
+                        ol_status = True
+                    line = f"<li>{ol.strip()}</li>\n"
+                if not ol_element and ol_status:
+                    w.write('</ol>\n')
+                    ol_status = False
+
+                # unordered list
+                if ul_element:
+                    if not ul_status:
+                        w.write('<ul>\n')
+                        ul_status = True
+                    line = f"<li>{ul.strip()}</li>\n"
+                if not ul_element and ul_status:
+                    w.write('</ul>\n')
+                    ul_status = False
+                
+                # paragraphs and line breaks
+                if not (hlevel or ol_status or ul_status):
+                    if not p_status and length > 1:
+                        w.write('<p>\n')
+                        p_status = True
+                    elif length > 1:
+                        w.write('<br/>\n')
+                    elif p_status:
+                        w.write('</p>\n')
+                        p_status = False
+            
+                if length > 1:
+                    w.write(line)
+
+            # close tags if open and EOF
+            if ol_status:
+                w.write('</ol>\n')
+            if ul_status:
+                w.write('</ul>\n')
+            if p_status:
+                w.write('</p>\n')
+
+    # exit with a success status code
+    exit(0)
